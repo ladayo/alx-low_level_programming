2a2cf065b7ddfd4424051aedb7b4ab32342748ee
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-15 16:42:38 +0000

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *s = "_putchar";
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+	return (0);
+}
