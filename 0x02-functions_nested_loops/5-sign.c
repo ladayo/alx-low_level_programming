b635e6d1e41d77b5c5552a4f079dd3d1329fc151
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-15 19:33:18 +0000

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_sign - ouputs the sign
+* @n: parameter n is the number
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
