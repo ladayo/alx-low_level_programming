a76733cc65ff94e8d5fbe7dfdb839fccba936529
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-15 18:14:10 +0000

    print the alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_alphabet_x10  -  outputs alphabets 10times
+*
+*
+* Return: Always 0 (Success)
+*
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
