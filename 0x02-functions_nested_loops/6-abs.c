25d83a7d48f6af39bcd4648ac56070c9214e8c58
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-15 19:57:27 +0000

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _abs - ouputs the positive
+*@i: parameter i is the number
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int _abs(int i)
+{
+	return (abs(i));
+}
