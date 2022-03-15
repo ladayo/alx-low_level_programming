638a69183947bf8b7c5acc6669be45f2e25e8eef
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-15 17:48:11 +0000

    print lower case alphabets correction 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,17 @@
 #include "main.h"
 
 /**
- * print_alphabet:  writes the lower case alphabets
- * 
- *
- * Return void
- * 
- */
+* print_alphabet -  outputs alphabets in lower case
+*
+*
+* Return: Always 0 (Success)
+*
+*/
 
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
