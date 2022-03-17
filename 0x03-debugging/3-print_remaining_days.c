5d6f139d9f3d0e220444ea7a963dd74a2977883d
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-17 21:21:26 +0000

    printing remaining days correction 4

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,7 +23,8 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month > 12 || month < 1 || day < 1 || day > 366)
+		if ((month > 12 || month < 1 || day < 1 || day > 366)
+			|| (day == 29 && month == 2))
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
