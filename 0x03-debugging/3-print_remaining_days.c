75773d9bc463c39c2ada02bd3a65469075d66a2a
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-17 21:02:08 +0000

    printing remaining days correction 2

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,41 +12,14 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0)
+	if (((year%4==0) && (year%100!=0)) || (year%400==0))
 	{
-		if (year % 100 == 0)
+		if (month >= 2 && day >= 60)
 		{
-			if (year % 400 == 0)
-			{
-				if (month >= 2 && day >= 60)
-				{
-					day++;
-				}
-				printf("Day of the year: %d\n", day);
-				printf("Remaining days: %d\n", 366 - day);
-			}
-			else
-			{
-				if (month > 12 || month < 1 || day < 1 || day > 366)
-				{
-					printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-				}
-				else
-				{
-					printf("Day of the year: %d\n", day);
-					printf("Remaining days: %d\n", 365 - day);
-				}
-			}
+			day++;
 		}
-		else
-		{
-			if (month >= 2 && day >= 60)
-			{
-				day++;
-			}
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
-		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
