a60c8085a219c9c54e4e8672b7955655a64994d5
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-21 20:25:53 +0000

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - length of string
+ *
+ * @s: required string
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
