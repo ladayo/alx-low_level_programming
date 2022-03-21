b11347f7101462358dadaa47debfdd3385224997
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-21 20:48:23 +0000

    prints strings and a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - print string
+ *
+ * @str: input string
+ *
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	puts("\n");
+}
