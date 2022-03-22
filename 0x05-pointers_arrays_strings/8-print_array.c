9291004899ffb833ffedcdedb18d9893cece4de2
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-22 03:56:36 +0000

    print values of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - print array elements
+ *
+ * @a: input array
+ * @n: number of elements
+ *
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <  n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+	putchar('\n');
+}
