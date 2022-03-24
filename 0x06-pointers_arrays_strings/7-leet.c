0b53ff10aa519d791273f7a7f0bf33255c0ff515
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-24 23:23:36 +0000

    encoding 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encoding str
+ *
+ * @s: str
+ *
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	char *a  = "aAeEoOtTiL";
+	char *b = "4433007711";
+	int i, j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (s[j] == a[i])
+			{
+				s[j] = b[i];
+			}
+
+		}
+	}
+	return (s);
+}
