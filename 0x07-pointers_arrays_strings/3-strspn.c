22038e964930dfbe1192a62a695113a16d5b6cac
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-28 19:03:15 +0000

    length of prestring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - length of prefix substring
+ *
+ * @s: string pointer
+ * @accept: search byte
+ *
+ * Return: int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, count = 0;
+
+	while (*(accept + i) != '\0')
+	{
+		for (j = 0; *(s + j) != '\0'; j++)
+		{
+			if (*(accept + i) == *(s + j))
+			{
+				count++;
+			}
+		}
+		accept++;
+	}
+	return (count);
+}
