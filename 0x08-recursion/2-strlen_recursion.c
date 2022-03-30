eca0bb85d88b9e22eec21877b88802c6efc8f178
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-30 22:29:20 +0000

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - print length of string
+ *
+ * @s: string
+ *
+ * Return: int
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+
+	return (len);
+}
