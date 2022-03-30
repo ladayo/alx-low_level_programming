4fb093c19933849251aecaf9a6b458d391418d0c
DayoVagrantVM <georgeoladayo@gmail.com>
2022-03-30 23:10:45 +0000

    power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - find the power of a number
+ *
+ * @x: value
+ * @y: power
+ *
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
+}
