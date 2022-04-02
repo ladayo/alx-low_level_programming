996a49987e349023f0fe9b020fe685ce7420a9fc
ladayo <66234219+ladayo@users.noreply.github.com>
2022-04-02 05:54:28 +0100

    multiply two numbers correction 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,24 +3,23 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers
- *
- * @argc: count
- * @argv: string array
- *
- * Return: Always 0
+ * main - multiply 2 numbers passed to main, or Error
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if error, 0 if function runs correctly
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc <= 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	(void) argc;
+
+	if (argv[1] && argv[2])
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
+	else
+		printf("Error\n");
+
+	return (1);
 }
