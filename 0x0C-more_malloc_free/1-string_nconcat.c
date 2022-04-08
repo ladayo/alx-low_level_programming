30e82e02f14850ef2a2fe50775c6d3fc711e0af2
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-08 01:27:30 +0000

    string concat correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,12 +31,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!ptr)
 		return (NULL);
 	for (i = 0; s1[i]; i++)
-		*(ptr + i) = *(s1 + i);
+		ptr[i] = s1[i];
 	for (j = 0; j <= n; j++)
 	{
-		*(ptr + i) = *(s2 + j);
+		ptr[i] = s2[j];
 		i++;
 	}
-	*(ptr + i) = '\0';
+	ptr[i] = '\0';
 	return (ptr);
 }
