26242de0e89f64e2f1a37925c0ec39a8e0e32093
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-08 01:40:06 +0000

    string concat correction 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,28 +14,27 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i, j, nstr;
+	unsigned int i, sizeS1, sizeS2, nstr;
 
 	if (!s1)
 		s1 = "";
 	if (!s2)
 		s2 = "";
-	for (i = 0; s1[i]; i++)
+	for (sizeS1 = 0; s1[sizeS1]; sizeS1++)
 		;
-	for (j = 0; s2[j]; j++)
+	for (sizeS2 = 0; s2[sizeS2]; sizeS2++)
 		;
-	if (n >= j)
-		n = j;
-	nstr = i + n + 1; /*+1 for the null terminating character*/
+	if (n >= sizeS2)
+		n = sizeS2;
+	nstr = sizeS1 + n + 1; /*+1 for the null terminating character*/
 	ptr = malloc(sizeof(char) * nstr);
 	if (!ptr)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < sizeS1; i++)
 		ptr[i] = s1[i];
-	for (j = 0; j <= n; j++)
+	for (; i < (sizeS1 + n); i++)
 	{
-		ptr[i] = s2[j];
-		i++;
+		ptr[i] = s2[i - sizeS1];
 	}
 	ptr[i] = '\0';
 	return (ptr);
