455cca233385a0ffc4f0851536838b41429df298
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-12 16:28:59 +0000

    print struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,5 +19,5 @@ void print_dog(struct dog *d)
 		printf("Age: (nil)\n");
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
