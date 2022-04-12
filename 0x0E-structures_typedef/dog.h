9c2aec1c19a7a191eac685bf0a232f7e54c0efa3
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-12 01:51:32 +0000

    dog properties in structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_PROPERTIES
+#define DOG_PROPERTIES
+/**
+ * struct dog - defines the xtics of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owwner of dog
+ *
+ * Description: define the properties of dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
