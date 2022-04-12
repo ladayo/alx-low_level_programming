3c47048f8186a54935d332ee48e709fc8c4a3b4f
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-12 05:51:52 +0000

    initialize a dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,10 +13,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-        struct dog *new_dog;
-	
+	struct dog *new_dog;
+
 	new_dog = d;
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	if (new_dog)
+	{
+		new_dog->name = name;
+		new_dog->age = age;
+		new_dog->owner = owner;
+	}
 }
