45f0db6fad9614dd3778435aedea078fafa4eefb
ladayo <66234219+ladayo@users.noreply.github.com>
2022-04-12 04:10:00 +0100

    make dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,10 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *new_dog;
-
-	new_dog = d;
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
