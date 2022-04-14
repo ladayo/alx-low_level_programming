687597e86dbeac2d977af317b3d8d0ac890b11a3
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-14 16:51:35 +0000

    print name with function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - pointer func to print name
+ *
+ * @name: name to print
+ * @f: func pointer
+ *
+ * Return: Always 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
