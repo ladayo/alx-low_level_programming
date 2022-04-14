c788d80693909223d0423335448722a22b3131b1
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-14 21:56:13 +0000

    function to search for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: list of array elements
+ * @size: size of array
+ * @cmp: fucntion pointer
+ *
+ * Return: integer value
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, result_index;
+
+	if (cmp == NULL || array == NULL)
+		result_index = -1;
+	if (size <= 0)
+		result_index = -1;
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) > 0)
+		{
+			result_index = i;
+			break;
+		}
+		else
+		{
+			result_index = -1;
+		}
+	}
+	return (result_index);
+}
