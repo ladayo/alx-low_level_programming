a3138fe32aaaf45e4f6341428eb2e61484d6458b
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-15 14:01:07 +0000

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (j != NULL)
 			printf("%s", j);
 		else
-			printf("nil");
+			printf("(nil)");
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
