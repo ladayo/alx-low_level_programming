c74851bb9c7e86db138d596720e8cb7a54010631
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-26 19:56:16 +0000

    free memory of nodes in list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - free type of listint list
+ * @head: pointer to first node in link
+ *
+ * Return: Always 0
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	ptr = head;
+	while(ptr->next != NULL)
+	{
+		ptr = ptr->next;
+		free(head);
+		head = ptr;
+	}
+	free(head);
+}
