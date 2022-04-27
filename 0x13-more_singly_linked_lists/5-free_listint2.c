ea7eab217dc3fc902cc28898b5abfeafc3f3f7d1
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-27 01:53:55 +0000

    free list and make head NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list and set head to NULL
+ * @head: adderess of head pointer
+ *
+ * Return: Always 0
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
