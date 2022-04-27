abb2135de8ab539d71fba6c90ea9a678f000e07e
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-27 04:37:01 +0000

    insert a node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,28 +11,38 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr, *forward, *new_node;
-	unsigned int count = 0;
+	unsigned int i;
+	listint_t *current_node, *new_node;
 
-	new_node = (struct listint_s *)malloc(sizeof(listint_t));
+	if (head == NULL)
+		return (NULL);
+
+	if (idx)
+	{
+		current_node = *head;
+		for (i = 0; i < idx - 1 && current_node != NULL; i++)
+		{
+			current_node = current_node->next;
+		}
+
+		if (current_node == NULL)
+			return (NULL);
+	}
+
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
-	ptr = *head;
-	if (idx - 1)
+	if (idx == 0)
 	{
-		while (count < (idx - 1))
-		{
-			ptr = ptr->next;
-			count++;
-		}
-		forward = ptr->next;
-		ptr->next = new_node;
-		new_node->next = forward;
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
-	else
-	{
-		return (NULL);
-	}
+
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+	return (new_node);
 }
