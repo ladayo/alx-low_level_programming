75141e0b94708aa3fe9b124af99d7ff94f675c56
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-27 03:23:50 +0000

    get node with given index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns node at a givin index
+ * @head: pointer to head
+ * @index: given node position
+ *
+ * Return: node of type listint_t
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+	if (index == 0 && head != NULL)
+		return (head);
+	while (count < index)
+	{
+		head = head->next;
+		if (head->next == NULL)
+			return (head);
+		count++;
+	}
+	return (head);
+}
