60d80512e74abd9d43c2f1068ccc8881d0979b4e
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-27 02:39:49 +0000

    delete head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete head of list
+ * @head: address of head pointer
+ *
+ * Return: head node data (n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int num;
+
+	if (*head == NULL)
+		return (1);
+	ptr = *head;
+	*head = (*head)->next;
+	num = ptr->n;
+	free(ptr);
+	return (num);
+}
