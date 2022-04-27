f6a55d69fdcffd60710af9df35e3ef0c01c5c083
DayoVagrantVM <georgeoladayo@gmail.com>
2022-04-27 03:30:17 +0000

    get node with given index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (count < index)
 	{
 		head = head->next;
-		if (head->next == NULL)
+		if (head == NULL)
 			return (NULL);
 		count++;
 	}
