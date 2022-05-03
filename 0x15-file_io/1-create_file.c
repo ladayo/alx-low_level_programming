229bed426fc59af86e21582e1e684190d2310a88
DayoVagrantVM <georgeoladayo@gmail.com>
2022-05-03 09:28:58 +0000

    create a file and write to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - create a new file and add permissions
+ * @filename: address of file to be created
+ * @text_content: address of txt
+ *
+ * Return: 1 on Success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+	int count = 0;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		creat(filename, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	for (count = 0; text_content[count] != '\0'; count++)
+		;
+	wr = write(fd, text_content, count);
+	if (wr < 0)
+		return (-1);
+	return (1);
+}
