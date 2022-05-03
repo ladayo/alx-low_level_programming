ef6ae8ecf67d179f139f468fc6fd78e4439966e7
DayoVagrantVM <georgeoladayo@gmail.com>
2022-05-03 17:02:03 +0000

    [6]fix error 13,15,16

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,15 +2,13 @@
 
 /**
  * main - copies the content of a file to another file
- * @argc: number of argument varibles
- * @argv: pointer to argument variables
+ * @ac: number of argument varibles
+ * @av: pointer to argument variables
  *
  * Return: 0 on success
  */
 
-int main(int argc, char *argv[])
-{
-	int main(int ac, char *av[])
+int main(int ac, char *av[])
 {
 	int fdf, fdt, sz, cf, ct;
 	char buf[1024];
@@ -20,14 +18,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fdf = open(av[1], O_RDONLY);
 	if (fdf == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	fdt = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fdt == -1)
 	{
@@ -35,7 +31,6 @@ int main(int argc, char *argv[])
 		close(fdf);
 		exit(99);
 	}
-
 	while ((sz = read(fdf, buf, 1024)))
 		write(fdt, buf, sz);
 	cf = close(fdf);
