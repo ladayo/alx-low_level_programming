45b59b39add52d85fc0bd8ab1528fb95d4ccee14
DayoVagrantVM <georgeoladayo@gmail.com>
2022-05-03 07:29:48 +0000

    fix checker error 3,4,5,9

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,16 +12,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, size, wr;
 	char *buff;
-
+	if (!filename)
+		return (0);
 	buff = malloc((sizeof(char) * letters));
 	if (!buff)
 		return (0);
 	fd = open(filename, O_RDWR);
-	if ((fd == -1) || (filename == NULL))
+	if ((fd == -1))
 		return (0);
 	size = read(fd, buff, letters);
 	wr = write(STDOUT_FILENO, buff, size);
-	if ((wr < 0) || (wr != size))
+	if ((size < 0) || (wr < 0) || (wr != size))
 		return (0);
 	return (wr);
 	close(fd);
