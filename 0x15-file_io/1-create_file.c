19c2ad70cee91c3b6d07690c050c0e4035238745
DayoVagrantVM <georgeoladayo@gmail.com>
2022-05-03 09:39:27 +0000

    [1]fix checker error 7

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,10 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
+	{
 		creat(filename, 0600);
+		return (1);
+	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	for (count = 0; text_content[count] != '\0'; count++)
 		;
