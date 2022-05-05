a35cb1f13ce16a1424adeb02e853d3b4681e4a16
DayoVagrantVM <georgeoladayo@gmail.com>
2022-05-05 18:19:05 +0000

    [10]fix checker error 13,15,16

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * error_check - check for reading and writing errors
+ * @fdrd: integer returned from read
+ * @fdwr: integer returned form writing
+ * @argv: pointer arguments to elements entered in terminal
+ *
+ * Return: void
+ */
+
+void error_check(int fdrd, int fdwr, char **argv)
+{
+	if (fdrd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (fdwr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write from file %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: number of argument varibles
  * @argv: pointer to argument variables
@@ -8,29 +31,27 @@
  * Return: 0 on success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int rd, rd_arg1 = 1024;
-	int wr, c_rd, c_wr;
+	int rd, wr, num_rd, num_wr, c_rd, c_wr;
 	char *file_content;
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	rd = open(argv[1], O_RDONLY);
-	if (rd == -1 || argv[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]), exit(98);
 	wr = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	error_check(rd, wr, argv);
 	file_content =  malloc(sizeof(char) * 1024);
 	if (!file_content)
 		return (-1);
-	while (rd_arg1 > 0)
+	while (sizeof(file_content) > 0)
 	{
-		rd_arg1 = read(rd, file_content, 1024);
-		if (rd == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]), exit(98);
-		write(wr, file_content, rd_arg1);
-		if (wr == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		num_rd = read(rd, file_content, 1024);
+		if (num_rd == -1)
+			error_check(-1, 0, argv);
+		num_wr = write(wr, file_content, num_rd);
+		if (num_wr == -1)
+			error_check(0, -1, argv);
 	}
 	c_rd = close(rd);
 	c_wr = close(wr);
