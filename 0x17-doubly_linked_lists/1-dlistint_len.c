54cdb1ada9102c2cb56aeb95f7301f93ca98c367
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-09 20:17:15 +0000

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - get the number of elements in a list
+ *
+ * @h: pointer to head of list
+ *
+ * Return: number of elements in doubly linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num_node = 0;
+
+	while(h)
+	{
+		num_node++;
+		h = h->next;
+	}
+	return (num_node);
+}
