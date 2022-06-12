3339414186ed2bdbf6d6df2d94325762b1ab58f6
DayoVagrantVM <georgeoladayo@gmail.com>
2022-06-12 18:25:28 +0000

    task 2 correction

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,22 +1,26 @@
 #include "lists.h"
 
 /**
- * add_dnode - add a node to D list
- * head: adderess of node to add
- * n: element in node
+ * add_dnodeint - add a node to D list
+ * @head: adderess of node to add
+ * @n: element in node
  *
  * Return: adderess of new element
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *n_head = malloc(sizeof(dlistint_t));
+
 	if (n_head == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
+
 	n_head->prev = NULL;
 	n_head->next = *head;
 	n_head->n = n;
+	if (*head)
+		(*head)->prev = n_head;
 	*head = n_head;
-	return n_head;
+	return (n_head);
 }
